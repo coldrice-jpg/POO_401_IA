bd9498325ce4763357c6b7beba7405d99cfb05e4
coldrice-jpg <al9817071@gmail.com>
2026-02-26 08:08:06 -0600

    Elementos Robot

---
+++ src/main/java/unidad1/elementos/robot/Brazo.java
@@ -0,0 +1,22 @@
+package main.java.unidad1.elementos.robot;
+
+public class Brazo {
+
+    private String tipoMovimiento;
+    private float velocidad;
+
+
+    // Metodos
+    public static void main(String[] args) {
+
+        Brazo brazo = new Brazo();
+
+        public void asignarMovimiento();
+
+        public void abrirGarra();
+
+        public void cerrarGarra();
+
+    }
+
+}
---
+++ src/main/java/unidad1/elementos/robot/Sensor.java
@@ -0,0 +1,15 @@
+package main.java.unidad1.elementos.robot;
+
+public class Sensor {
+
+    private boolean activo;
+
+    public static void main(String[] args) {
+
+        Sensor sensor = new Sensor();
+
+        public float leerValor();
+
+    }
+
+}
