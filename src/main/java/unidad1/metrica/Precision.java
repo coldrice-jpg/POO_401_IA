a3d0a1d334def1fbc0eaf14553b0e0300048f5d2
coldrice-jpg <al9817071@gmail.com>
2026-03-06 07:23:13 -0600

    Practica Metrica

---
+++ src/main/java/unidad1/metrica/Precision.java
@@ -0,0 +1,9 @@
+package main.java.unidad1.metrica;
+
+public class Precision extends Metrica{
+
+    @Override
+    public void calcularResultados() {
+        System.out.println("Calculando Precision...");
+    }
+}
