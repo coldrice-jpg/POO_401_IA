d7c0bb2034c94d8663c1016acf0d85496ec296e1
coldrice-jpg <al9817071@gmail.com>
2026-03-12 08:42:10 -0600

    Practica OptimizadorIA

---
+++ src/main/java/unidad1/orquestacion/polimorfica/DescensoGradiente.java
@@ -0,0 +1,9 @@
+package main.java.unidad1.orquestacion.polimorfica;
+
+public class DescensoGradiente extends OptimizadorIA {
+
+    @Override
+    public void ajustarParametros() {
+        System.out.println("Calculando derivadas parciales y actualizando pesos...");
+    }
+}
--- src/main/java/unidad1/pipiline/procesamiento/Main.java
+++ src/main/java/unidad1/pipiline/procesamiento/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-        public void main(String[] args) {
+        public static void main(String[] args) {
 
             List<FiltroImagen> pipeline = new ArrayList<>();
 
