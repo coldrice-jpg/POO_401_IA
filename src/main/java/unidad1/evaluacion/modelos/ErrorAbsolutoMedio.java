07a32696fda0cf727de49f6717db7cd991d3ec87
coldrice-jpg <al9817071@gmail.com>
2026-03-13 09:04:40 -0600

    Evaluacion Modelos

---
+++ src/main/java/unidad1/evaluacion/modelos/ErrorAbsolutoMedio.java
@@ -0,0 +1,9 @@
+package main.java.unidad1.evaluacion.modelos;
+
+public class ErrorAbsolutoMedio extends EvaluadorMetricas {
+
+    @Override
+    public void calcularMetodo() {
+        System.out.println("El error absoluto es...");
+    }
+}
