83a387123246ddbfdc0b830602413e17b702339e
coldrice-jpg <al9817071@gmail.com>
2026-03-13 08:08:15 -0600

    Procesamiento Senales

---
+++ src/main/java/unidad1/procesamiento/senales/ReductorEco.java
@@ -0,0 +1,9 @@
+package main.java.unidad1.procesamiento.senales;
+
+public class ReductorEco extends ProcesadorDeAudio {
+
+    @Override
+    public void filtrar() {
+        System.out.println("Reduciendo eco...");
+    }
+}
