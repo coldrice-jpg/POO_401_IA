e504d5d6e3959a0e0722b5632594bebae122b3ae
coldrice-jpg <al9817071@gmail.com>
2026-03-20 07:51:04 -0600

    Practica 04

---
+++ src/main/java/unidad2/abstraccion/sensores/SensorLidar.java
@@ -0,0 +1,9 @@
+package unidad2.abstraccion.sensores;
+
+public class SensorLidar extends SensorIA {
+
+    @Override
+    public void leerDatos() {
+        System.out.println("Escaneando entorno 360°... Generando nube de puntos láser.");
+    }
+}
