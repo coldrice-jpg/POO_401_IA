2b87ac3c6eada9cbd6ec7469084a2119139ba2ea
coldrice-jpg <al9817071@gmail.com>
2026-03-26 07:34:32 -0600

    Procesador Analisis de Sentimientos

--- src/main/java/unidad2/abstraccion/procesadores/ProcesadorAnalisisSentimiento.java
+++ src/main/java/unidad2/abstraccion/procesadores/ProcesadorAnalisisSentimiento.java
@@ -1,7 +1,38 @@
 package unidad2.abstraccion.procesadores;
 
-public class ProcesadorAnalisisSentimiento {
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ProcesadorAnalisisSentimiento extends ProcesadorNLP {
+
+    private static final List<String> STOP_WORDS = Arrays.asList("el", "la", "un");
+
+    @Override
+    public List<String> tokenizar() {
+        tokens = new ArrayList<>(Arrays.asList(getTextoCrudo().split("\\s")));
+        return tokens;
+    }
+
+    @Override
+    public void limpiarTexto() {
+
+        tokens = tokens.stream().filter(p -> !STOP_WORDS.contains(p.toLowerCase())).collect(Collectors.toList());
+
+    }
+
+    @Override
+    public Object transformarParaModelo() {
+
+        Map<String, Integer> frecuencia = new HashMap<>();
+        for (String palabra: tokens) {
+            if (tokens.contains(palabra.toLowerCase())) {
+                frecuencia.merge(palabra, 1, Integer::sum);
 
 
+            }
+        }
 
+        return frecuencia.toString();
+
+    }
 }
